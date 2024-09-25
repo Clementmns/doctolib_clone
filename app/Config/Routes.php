40e9ec37aa64664e1f6d935eacb5cf5c945ffe43
makelia <bysxshy@gmail.com>
2024-09-25 10:48:30 +0200

    ajout de la route

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -7,5 +7,6 @@ use CodeIgniter\Router\RouteCollection;
  */
 $routes->get('/', 'Home::index');
 $routes->get('patients', 'Patients::index');
+$routes->get('etablishments', 'Etablishments::index');
 
 $routes->get('specialites', 'Specialites::index');
