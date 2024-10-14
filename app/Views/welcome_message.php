51d86f2d1b413b6b62f6ab1418d9e7d331509721
Titouan Lahchiouach <lahchiouach.titouan@gmail.com>
2024-10-14 16:02:16 +0200

    style page welcome

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -1,46 +1,43 @@
-<header>
-    <h1>Liste des fonctionnalités de "Mon Doctolib"</h1>
-</header>
 <ul>
     <li>
         <b>Consultations simples de la BDD (tables sans clés étrangères)</b>
-        <ul>
-            <li><a href="patients/" class="hover:underline text-blue-600">Affichage de la liste des patients</a></li>
-            <li><a href="specialities/" class="hover:underline text-blue-600">Affichage de la liste des spécialités</a></li>
-            <li><a href="etablishments/" class="hover:underline text-blue-600">Affichage de la liste des établissements</a></li>
-            <li><a href="practitioners/" class="hover:underline text-blue-600">Affichage de la liste des praticiens</a></li>
+        <ul class="flex gap-10">
+            <li class="w-[150px] h-[150px] border"><a href="patients/" class="hover:underline text-blue-600">Affichage de la liste des patients</a></li>
+            <li class="w-[150px] h-[150px] border"><a href="specialities/" class="hover:underline text-blue-600">Affichage de la liste des spécialités</a></li>
+            <li class="w-[150px] h-[150px] border"><a href="etablishments/" class="hover:underline text-blue-600">Affichage de la liste des établissements</a></li>
+            <li class="w-[150px] h-[150px] border"><a href="practitioners/" class="hover:underline text-blue-600">Affichage de la liste des praticiens</a></li>
         </ul>
     </li>
     <li>
         <b>Alimentations simples de la BDD (tables sans clés étrangères)</b>
-        <ul>
-            <li><a href="practitioners/new" class="hover:underline text-blue-600">Création d'un praticien</a></li>
-            <li><a href="" class="hover:underline text-blue-600">Création d'un établissement</a></li>
+        <ul class="flex gap-10">
+            <li class="w-[150px] h-[150px] border"><a href=" practitioners/new" class="hover:underline text-blue-600">Création d'un praticien</a></li>
+            <li class="w-[150px] h-[150px] border"><a href="" class=" hover:underline text-blue-600">Création d'un établissement</a></li>
         </ul>
     </li>
     <li>
         <b>Alimentations plus complexes de la BDD</b>
-        <ul>
-            <li><a href="" class="hover:underline text-blue-600 ">Association praticien-spécialité</a></li>
-            <li><a href="" class="hover:underline text-blue-600">Création d'un patient</a></li>
-            <li><a href="" class="hover:underline text-blue-600">Définir le lieu de pratique d'un praticien donné</a></li>
-            <li><a href="" class="hover:underline text-blue-600">Création d'un rendez-vous entre un patient et un praticien</a></li>
+        <ul class="flex gap-10">
+            <li class="w-[150px] h-[150px] border"><a href="" class=" hover:underline text-blue-600 ">Association praticien-spécialité</a></li>
+            <li class="w-[150px] h-[150px] border"><a href="" class=" hover:underline text-blue-600">Création d'un patient</a></li>
+            <li class="w-[150px] h-[150px] border"><a href="" class="hover:underline text-blue-600">Définir le lieu de pratique d'un praticien donné</a></li>
+            <li class="w-[150px] h-[150px] border"><a href="" class="hover:underline text-blue-600">Création d'un rendez-vous entre un patient et un praticien</a></li>
         </ul>
     </li>
     <li>
         <b>Consultations plus complexes de la BDD</b>
-        <ul>
-            <li><a href="" class="hover:underline text-blue-600">Affichage de la liste des praticiens selon la sélection de leur spécialité</a></li>
-            <li><a href="" class="hover:underline text-blue-600">Consultation des rdv d'un praticien</a></li>
-            <li><a href="" class="hover:underline text-blue-600">Consultation des rdv d'un patient</a></li>
+        <ul class="flex gap-10">
+            <li class="w-[150px] h-[150px] border"><a href="" class="hover:underline text-blue-600">Affichage de la liste des praticiens selon la sélection de leur spécialité</a></li>
+            <li class="w-[150px] h-[150px] border"><a href="" class="hover:underline text-blue-600">Consultation des rdv d'un praticien</a></li>
+            <li class="w-[150px] h-[150px] border"><a href="" class="hover:underline text-blue-600">Consultation des rdv d'un patient</a></li>
         </ul>
     </li>
     <li>
         <b>Modifications de la base de donnée</b>
-        <ul>
-            <li><a href="" class="hover:underline text-blue-600">Modifier les données temporelles d'un rendez-vous</a></li>
-            <li><a href="" class="hover:underline text-blue-600">Modifier le praticien du rendez-vous</a></li>
-            <li><a href="" class="hover:underline text-blue-600">Annuler un rendez-vous</a></li>
+        <ul class="flex gap-10">
+            <li class="w-[150px] h-[150px] border"><a href="" class="hover:underline text-blue-600">Modifier les données temporelles d'un rendez-vous</a></li>
+            <li class="w-[150px] h-[150px] border"><a href="" class="hover:underline text-blue-600">Modifier le praticien du rendez-vous</a></li>
+            <li class="w-[150px] h-[150px] border"><a href="" class="hover:underline text-blue-600">Annuler un rendez-vous</a></li>
         </ul>
     </li>
 </ul>
