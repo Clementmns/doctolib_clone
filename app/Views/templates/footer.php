9f4262421d9c3159e3bac23c6208005952c6f244
Titouan Lahchiouach <lahchiouach.titouan@gmail.com>
2024-11-12 14:45:30 +0100

    style footer

--- app/Views/templates/footer.php
+++ app/Views/templates/footer.php
@@ -1,3 +1,6 @@
-   <em>&copy; Mon doctolib Version 1.0 2023</em>
-</body>
-</html>
+  <footer class="bg-[#117ACA] flex justify-center p-6 rounded-t-xl mt-4 items-center w-screen">
+     <em class="text-white">&copy; Mon doctolib Version 1.0 2023</em>
+</footer>
+  </body>
+
+  </html>
