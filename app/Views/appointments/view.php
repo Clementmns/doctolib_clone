3c00ee47ecb5fe9872e14ef117cc71af2ef4f216
Clément Omnès <clementgoni@gmail.com>
2024-11-13 10:33:52 +0100

    fix

--- app/Views/appointments/view.php
+++ app/Views/appointments/view.php
@@ -51,7 +51,6 @@
                 Appliquer
             </button>
         </form>
-        <a class="bg-[#117ACA] text-white rounded p-2 hover:bg-[#00264C] transition duration-200" href="<?= base_url('appointment/new'); ?>">Ajouter un rdv</a>
     </div>
 
     <div id='calendar'></div>
